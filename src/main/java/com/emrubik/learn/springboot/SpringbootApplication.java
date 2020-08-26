d017f4a5ce5fba9fc739c58f2a3264142b080872
马誌浩 <mazh@emrubik.com>
2020-08-26 13:59:45 +0800

    #ehcache 配置

--- src/main/java/com/emrubik/learn/springboot/SpringbootApplication.java
+++ src/main/java/com/emrubik/learn/springboot/SpringbootApplication.java
@@ -2,24 +2,20 @@ package com.emrubik.learn.springboot;
 
 import com.emrubik.learn.springboot.config.DBConfig1;
 import com.emrubik.learn.springboot.config.DBConfig2;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.PropertiesPropertySource;
-import org.springframework.core.env.PropertySource;
-import org.springframework.core.io.support.EncodedResource;
-import org.springframework.core.io.support.PropertySourceFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
+import javax.cache.CacheManager;
+import javax.cache.configuration.MutableConfiguration;
+
+import javax.cache.expiry.Duration;
+import javax.cache.expiry.TouchedExpiryPolicy;
+import java.util.concurrent.TimeUnit;
+
 
 @EnableCaching // 开启缓存注解
 //可以在配置类里面使用 EnableConfigurationProperties  也可以  @Configuration
@@ -33,6 +29,14 @@ public class SpringbootApplication {
         SpringApplication.run(SpringbootApplication.class, args);
     }
 
+    //@Bean
+    public JCacheManagerCustomizer jCacheManagerCustomizer(){
 
+        return cacheManager -> cacheManager.createCache("userCache1", new MutableConfiguration<>()
+                .setExpiryPolicyFactory(TouchedExpiryPolicy.factoryOf(new Duration(TimeUnit.MINUTES, 30)))
+                .setStoreByValue(false)
+                .setStatisticsEnabled(true));
+    }
+    //https://www.bbsmax.com/A/KE5QKAwkzL/
 
 }
